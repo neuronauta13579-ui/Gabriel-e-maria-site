93bf5c067d4e53fa9d83d556639996f5d4ed5680
neuronauta13579-ui <neuronauta13579@gmail.com>
2026-07-29 12:42:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function maisGabriel() {
+  alert("Gabriel");
+}
+
+function menosGabriel() {
+  alert("Menos Gabriel");
+}
+
+function maisMaria() {
+  alert("Maria");
+}
+
+function menosMaria() {
+  alert("Menos Maria");
+}
+
+function salvar() {
+  alert("Salvou!");
+}
